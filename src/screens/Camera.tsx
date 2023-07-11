0f7143a11cb747f7463dde267e40fd3a6bc8fd37
xdiegors <xdiegors@outlook.com>
2023-07-11 09:45:41 -0300

    Camera component reading QR Code

--- src/screens/Camera.tsx
+++ src/screens/Camera.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {CameraScreen} from 'react-native-camera-kit';
 
 import {Alert, View} from 'react-native';
 
-export function Camera() {
+export function Camera({navigation}) {
+  const [scanning, setScanning] = useState(false);
+
+  const readQRCode = event => {
+    Alert.alert(event.nativeEvent.codeStringValue);
+    setScanning(false);
+    navigation.navigate('Home');
+  };
+
+  useEffect(() => {
+    setScanning(true);
+  }, []);
+
   return (
     <View
       style={{
@@ -12,23 +24,13 @@ export function Camera() {
       <CameraScreen
         actions={{rightButtonText: 'Done', leftButtonText: 'Cancel'}}
         onBottomButtonPressed={event => this.onBottomButtonPressed(event)}
-        // flashImages={{
-        //   // optional, images for flash state
-        //   on: require('path/to/image'),
-        //   off: require('path/to/image'),
-        //   auto: require('path/to/image'),
-        // }}
-        //cameraFlipImage={require('path/to/image')} // optional, image for flipping camera button
-        // captureButtonImage={require('path/to/image')} // optional, image capture button
-        //  torchOnImage={require('path/to/image')} // optional, image for toggling on flash light
-        //  torchOffImage={require('path/to/image')} // optional, image for toggling off flash light
-        hideControls={false} // (default false) optional, hides camera controls
-        showCapturedImageCount={false} // (default false) optional, show count for photos taken during that capture session
-        scanBarcode={true}
-        onReadCode={event => Alert.alert('QR code found')} // optional
-        showFrame={true} // (default false) optional, show frame with transparent layer (qr code or barcode will be read on this area ONLY), start animation for scanner,that stoped when find any code. Frame always at center of the screen
-        laserColor="blue" // (default red) optional, color of laser in scanner frame
-        frameColor="white" //
+        hideControls={false}
+        showCapturedImageCount={false}
+        scanBarcode={scanning}
+        onReadCode={event => readQRCode(event)}
+        showFrame={true}
+        laserColor="red"
+        frameColor="white"
       />
     </View>
   );
